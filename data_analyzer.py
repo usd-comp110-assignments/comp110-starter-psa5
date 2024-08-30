2734318965084a0dfe5b43ff3c823a59d8f89d2a
Sophia Krause-Levy <skrauselevy@sandiego.edu>
2024-08-30 10:12:31 -0700

    adding starter code

---
+++ data_analyzer.py
@@ -0,0 +1,66 @@
+"""
+Module: data_analyzer
+
+Module that allows us to import, analyze, and graph data that has a linear
+relationship.
+
+Authors:
+1) Name - USD Email Address
+2) Name - USD Email Address
+"""
+
+import matplotlib.pyplot as pp
+
+def separate(data_points):
+    """
+    Put your docstring comment here.
+    """
+
+    return None, None     # This is a placeholder line. You should replace it.
+
+
+def regression_slope(data_points):
+    """
+    Put your docstring comment here.
+    """
+
+    return 0.0  # This is a placeholder line. You should replace it.
+
+
+def plot_regression(data_points):
+    """
+    Put your docstring comment here.
+    """
+
+    return  # This is a placeholder line. You should replace it.
+
+
+def plot_data(data_points):
+    """
+    Put your docstring comment here.
+    """
+
+    return  # This is a placeholder line. You should replace it.
+
+
+def get_data(filename):
+    """
+    Put your docstring comment here.
+    """
+
+    return None, None, None  # This is a placeholder line. You should replace it.
+
+
+def main():
+    """
+    Put your docstring comment here.
+    """
+
+    data_filename = input("Enter the name of the data file: ")
+
+
+"""
+WARNING: Do NOT modify anything below this point, sp23.
+"""
+if __name__ == "__main__":
+    main()
